be23349e936298b5c257e14a64f614f8f93df762
Jakob <jakob.wolitzki@gmail.com>
2018-11-17 16:11:09 +0100

    Databasev2

--- back/HackhatonDinder/dinder/models.py
+++ back/HackhatonDinder/dinder/models.py
@@ -1,17 +1,22 @@
 from django.db import models
+from  django.contrib.auth.models import User
 
 # Create your models here.
 
 class DinderGroup(models.Model):
     name = models.CharField(max_length=100)
+    is_private=models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
 
 
+
 class DinderEvent(models.Model):
     name = models.CharField(max_length=100)
     group = models.ForeignKey(DinderGroup,on_delete=models.CASCADE)
+    localization = models.CharField(max_length=100,null=True,blank=True)
+
     def __str__(self):
         return self.name
 
@@ -19,7 +24,9 @@ class DinderEvent(models.Model):
 class DinderProfile(models.Model):
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
+    image = models.CharField(max_length=500,null=True,blank=True)
     groups = models.ManyToManyField(DinderGroup,null=True,blank=True)
     events = models.ManyToManyField(DinderEvent,null=True,blank=True)
+    localization = models.CharField(max_length=100,null=True,blank=True)
     def __str__(self):
         return self.name +' ' +self.surname
