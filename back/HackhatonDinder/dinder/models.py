ac2f764724f25082f301c78e1597b9852c9fb25b
Jakob <jakob.wolitzki@gmail.com>
2018-11-17 13:53:05 +0100

    Database v1

--- back/HackhatonDinder/dinder/models.py
+++ back/HackhatonDinder/dinder/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class DinderGroup(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class DinderEvent(models.Model):
+    name = models.CharField(max_length=100)
+    group = models.ForeignKey(DinderGroup,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+
+class DinderProfile(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    groups = models.ManyToManyField(DinderGroup,null=True,blank=True)
+    events = models.ManyToManyField(DinderEvent,null=True,blank=True)
+    def __str__(self):
+        return self.name +' ' +self.surname
