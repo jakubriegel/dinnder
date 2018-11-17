b9e5d4b7e2600d96d5315c3cb5b30ecaf28226f1
Jakob <jakob.wolitzki@gmail.com>
2018-11-17 19:06:39 +0100

    DatabaseQueries

--- back/HackhatonDinder/dinder/urls.py
+++ back/HackhatonDinder/dinder/urls.py
@@ -3,4 +3,10 @@ from . import views
 urlpatterns = [
 
     path('', views.index,name='index'),
+    path('api/login/<username>',views.loginView),
+    path('api/login/<username>',views.logoutView),
+    path('api/<uderid>/groups',views.showGroups),
+    path('api/<userid>/groups/<groupname>',views.showEvents),
+    path('api/<userid>/groups/<groupname>/events/<eventid>',views.showPeopleOfEvent),
+
 ]
--- back/HackhatonDinder/dinder/views.py
+++ back/HackhatonDinder/dinder/views.py
@@ -1,5 +1,42 @@
 from django.http import HttpResponse
 
+from .models import *
 
 def index(request):
     return HttpResponse("Dinder home.")
+
+
+def loginView(request, username):
+
+    print('not implemented login')
+
+    return HttpResponse("not implemented login")
+
+
+def logoutView(request, username):
+    print('not implemented logout')
+
+    return HttpResponse("not implemented logout")
+
+def showGroups(request,userid):
+    guy = DinderProfile.objects.get(id=userid)
+    guyGroups = guy.groups.all()
+    #object with groups of guy
+    pass
+
+def showEvents(request,userid,groupname):
+    guy = DinderProfile.objects.get(id=userid)
+    guyGroups = guy.groups.all()
+    group = guyGroups.get(name=groupname)
+    events = group.dinderevent_set.all()
+    #objects with events of a guy's chosen group
+
+def showPeopleOfEvent(request,userid,groupname,eventid):
+    guy = DinderProfile.objects.get(id=userid)
+    guyGroups = guy.groups.all()
+    group = guyGroups.get(name=groupname)
+    events = group.dinderevent_set.all()
+    event = events.get(id=eventid)
+    people = (event.dinderprofile_set.all())
+    print(people)
+    return HttpResponse("not implemented yet")
