caff1989bdd81c337eecfa400effb4af1fc70d7b
jakubriegel <jakubriegel@hotmail.com>
2018-11-18 06:03:39 +0100

    nearby

--- front/src/views/Groups.vue
+++ front/src/views/Groups.vue
@@ -2,6 +2,15 @@
     <div id="groups" class="groups container">
         <div class="row">
             <div class="col s12 m4">
+                <ul class="collection z-depth-4">
+                    <li class="collection-item avatar" id="nearby-container" v-on:click="getNearby()">
+                        <img src="https://i1.wp.com/www.123freevectors.com/wp-content/uploads/new/123fv-images/1297-google-maps-pin-icon.jpg?zoom=2.625&quality=100&strip=all&ssl=1" alt="photo" class="circle max-50">
+                        <p>
+                            <span class="title"><b>nearby</b></span> <br>
+                            Find some hungry folks nearby!
+                        </p>
+                    </li>
+                </ul>
                 <ul class="collection z-depth-4" id="groups_list">
                     <li class="collection-item avatar" v-for="group in groups" v-on:click="sendQuery(group.id)" :class="{orange:group.id == selected}" @click="selected = group.id">
                         <img v-bind:src="group.image" alt="photo" class="circle max-50">
@@ -54,12 +63,21 @@
         },
         methods: {
             sendQuery(id){
+                console.log("gropus query" + id);
                 axios
                     .get('http://10.254.50.28:8000/api/4/groups/' + id)
                     .then(response => (this.events = response.data.events))
 
                 $(".active").removeClass("orange active");
                 if ($(window).width() < 601){ window.location.replace("/events/" + id); }
+            },
+            getNearby() {
+                console.log("gropus near");
+                axios
+                    .get('http://10.254.50.28:8000/api/4/nearby/52.399455396/16.930157362')
+                    .then(response => (this.events = response.data.events))
+                $(".active").removeClass("orange active");
+                if ($(window).width() < 601){ window.location.replace("/events/" + 0); }
             }
         },
         mounted () {
